db5f62adde5fcd68b0c44e2e8e9f7770ae1a4dfa
walsh <walsh.zhang@hotmail.com>
2020-05-19 14:30:58 +0800

    Init Version

---
+++ introduction/src/main/java/ilearn/spring/security/IntroductionApplication.java
@@ -0,0 +1,11 @@
+package ilearn.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IntroductionApplication.class);
+    }
+}
