4017f572e2715dc77595195e20415f5463c80235
walsh <walsh.zhang@hotmail.com>
2020-05-22 20:18:05 +0800

    remove security configuration

--- introduction/src/main/java/ilearn/spring/security/AppSecurityConfiguration.java
+++ introduction/src/main/java/ilearn/spring/security/AppSecurityConfiguration.java
@@ -1,9 +1,8 @@
 package ilearn.spring.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class AppSecurityConfiguration  {
     
 }
